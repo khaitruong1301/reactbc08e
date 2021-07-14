ddd70a610f1766f48edafa10250a23df591d6cef
macbook <khaitruong1301@gmail.com>
2021-07-14 21:57:28 +0700

    buoi2_component_style_event_state

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import FunctionComponent from "./Components/FunctionComponent";
+// import ClassComponent from "./Components/ClassComponent";
+// import HomeComponent from "./Components/BaiTapLayoutComponent/HomeComponent";
+// import Databinding from "./Databinding/Databinding"
+// import BaiTapThucHanhLayout from "./Components/BaiTapThucHanhLayout/BaiTapThucHanhLayout";
+// import Demo from "./Databinding/Demo";
+// import HandleEvent from "./HandleEvent/HandleEvent";
+// import StyleComponent from "./StyleComponent/StyleComponent";
+import StateDemo from "./StateDemo/StateDemo"
+function App() {
+  return (
+    <div className="App">
+          {/* <HomeComponent /> */}
+          {/* <Databinding /> */}
+          {/* <BaiTapThucHanhLayout /> */}
+          {/* <Demo />  */}
+          {/* <HandleEvent></HandleEvent> */}
+
+          {/* <StyleComponent /> */}
+          {/* <p className="fontBold">ahihi</p> */}
+          <StateDemo />
+     
+    </div>
+  );
+}
+
+export default App;
+
+
